e02104b4a7d67b3edeece000aa2b9656bb067967
Tiffany Chimal <tiffany.chimal@hotmail.com>
2018-10-30 23:54:09 -0500

    Finished blogz

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'string'
@@ -15,17 +15,20 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(800))
     created = db.Column(db.DateTime)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title 
         self.body = body
         self.created = datetime.utcnow()
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -33,7 +36,7 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register']
+    allowed_routes = ['login', 'register', 'blog', 'index']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
 
@@ -46,9 +49,10 @@ def login():
         if user and user.password == password:
             session['email'] = email
             flash("Logged in")
-            return redirect('/blog')
+            return redirect('/newpost')
         else: 
             flash('User password incorrect, or user does not exist', 'error') 
+            return redirect('/login')
 
     return render_template('login.html')
 
@@ -67,7 +71,7 @@ def register():
             db.session.add(new_user)
             db.session.commit()
             session['email'] = email
-            return redirect('/')
+            return redirect('/blog')
         else:
             # TODO - user better response messaging
             return "<h1>Duplicate user</h1>"
@@ -77,17 +81,36 @@ def register():
 @app.route('/logout')
 def logout():
     del session['email']
-    return redirect('/')
+    return redirect('/blog')
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    users = User.query.all()
+    return render_template('index.html', title='Home', users=users)
 
 @app.route('/blog', methods=['GET'])
 def blog():
     if request.args.get("blog"):
         blog = Blog.query.get(request.args.get("blog"))
         return render_template('blogpost.html', blog=blog)
+
+    if request.args.get("user"):
+        user = User.query.get("email")
+        blogs = Blog.query.filter_by(id=user).first()
+        return render_template('SingleUser.html', user=user, blogs=blogs)   
+
     # get blogs from database
     blogs = Blog.query.all()
     # return template with blogs
-    return render_template('blogs.html', blogs=blogs)
+    return render_template('blogs.html', blogs=blogs) 
+
+# @app.route('/singleuser', methods=['POST', 'GET'])
+# def showuser():
+#     blog_name = request.form['title']
+#     owner = User.query.filter_by(email=session['email']).first()
+#     user_id = request.args.get('users')
+#     blogs = Blog.query.filter_by(username=user_id).all()
+#     return render_template('singleuser.html', users=users, blogs=blogs)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
@@ -109,7 +132,8 @@ def validate_new_post():
         body_error = 'Please fill in the blog post'
 
     if not title_error and not body_error:
-        new_blog = Blog(title, body)
+        owner = User.query.filter_by(email=session['email']).first()
+        new_blog = Blog(title, body, owner)
         db.session.add(new_blog)
         db.session.commit()
         return render_template('blogpost.html', blog=new_blog)
